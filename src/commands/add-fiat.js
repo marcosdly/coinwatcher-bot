27f6711ead72e22870b46aa2604a6098dca6de92
Marcos Gabriel <marcosdly@pm.me>
2022-01-10 11:19:15 -0300

    Finished add-fiat command to test adding data to db

--- src/commands/add-fiat.js
+++ src/commands/add-fiat.js
@@ -1,5 +1,41 @@
 'use strict';
 
 module.exports.execute = async function (message, fiat) {
-  await message.reply(`Fiat currency (${fiat}) added to the list.`);
+  const db = require('../mysql/sequelize/models/index');
+  const models = db.sequelize.models;
+  
+  let alrearyExists, fiat_to_show;
+  
+  await models.currency_configs.findByPk(String(message.guildId), {
+    attributes: [ 'fiat_to_show' ]
+  })
+  .then(data => {
+    fiat_to_show = data.dataValues.fiat_to_show;
+    if (fiat_to_show === null) {
+      alrearyExists = false;
+    } else {
+      alrearyExists = fiat_to_show.includes(fiat);
+    }
+  })
+  .catch(err => console.log(err));
+
+  if (alrearyExists) {
+    message.reply('Fiat already in list.');
+  } else {
+    if (fiat_to_show === null) {
+      fiat_to_show = fiat;
+    } else {
+      fiat_to_show += `,${fiat}`;
+    }
+
+    await models.currency_configs.update({
+      fiat_to_show: fiat_to_show
+    }, {
+      where: { server_id: String(message.guildId) },
+      fields: ['fiat_to_show']
+    })
+    .catch(err => console.log(err));
+
+    await message.reply(`Fiat currency (${fiat}) added to the list.\nCurrently selected fiats: ${fiat_to_show.split(',').join(', ')}`);
+  }
 }
