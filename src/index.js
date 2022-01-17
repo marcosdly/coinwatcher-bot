ef4d1d1c9a2e05f45abf6a08da921778d2cc31f5
Marcos Gabriel <marcosdly@pm.me>
2022-01-17 12:30:36 -0300

    Revert "Alteração de caminho de arquivo"
    This reverts commit 044cbed637305322ba8d1f1d9198333481f4885b.

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'))
 
 for (const file of commandFiles) {
-  const command = require(`./src/commands/${file}`);
+  const command = require(`./commands/${file}`);
 
   // Set a new item in the Collection with the key as
   // the command name and the value as the exported module
